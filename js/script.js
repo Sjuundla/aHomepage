d74a1f688fa0cd99f10f89362e4acc72182e3c03
Sjuundla <lee@leemail.xyz>
2018-11-25 00:37:50 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ var svgTrash   = "<svg style=\"width:24px;height:24px\" xmlns=\"http://www.w3.or
 // Also yes I could totally use a json object to represent the menus, but I didn't feel like reprogramming the whole script. Probably doing that next site, though.
 var linkMenu = [
   [svgTrash,                   "blue",                                        "-HEAD-"], // Anime
-  ["AnimeNewsNetwork",         "",""],
+  ["lgbt.io",         "http://lgbt.io",""],
   ["MyAnimeList",              "",""],
   ["Nyaa Tracker",             "",""],
   ["BakaBT",                   "",""],
